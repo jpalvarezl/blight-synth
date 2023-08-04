4da25579df6db504d1acfea2a0b60200e4ed50bb
Jose Alvarez <jp.alvarezl@gmail.com>
2023-08-04 10:49:20 -0700

    Listing available devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use cpal::traits::{HostTrait, DeviceTrait};
+
+fn main() -> Result<(), anyhow::Error>{
+    let available_hosts = cpal::available_hosts();
+    for host_id in available_hosts {
+        println!("Found host: {}", host_id.name());
+        let host = cpal::host_from_id(host_id)?;
+
+        for device in host.devices()? {
+            println!("Found device: {}", device.name()?);
+        }
+    }
+    Ok(())
 }
