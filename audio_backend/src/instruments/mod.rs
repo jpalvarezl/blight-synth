c251179a79ae65b3825e2be1d9af4be376787806
Jose Alvarez <jp.alvarezl@gmail.com>
2025-10-11 22:21:07 +0200

    Instruments are typealiases now

--- audio_backend/src/instruments/mod.rs
+++ audio_backend/src/instruments/mod.rs
@@ -12,9 +12,50 @@ pub use polyphonic_osc::*;
 pub use snare_drum::*;
 pub use synth_nodes::*;
 
-use crate::{id::NoteId, SynthNode, Voice};
+use crate::{id::NoteId, InstrumentTrait, MonoEffect, SynthNode, Voice, VoiceTrait};
 
 struct VoiceSlot<S: SynthNode> {
     inner: Voice<S>,
     note_id: Option<NoteId>, // This is the MIDI value of the note we use for identifying which voice is playing it.
+}
+
+pub struct MonophonicInstrument<S: SynthNode> {
+    instrument_id: crate::id::InstrumentId,
+    voice: VoiceSlot<S>,
+}
+
+impl<S: SynthNode> InstrumentTrait for MonophonicInstrument<S> {
+    fn id(&self) -> crate::id::InstrumentId {
+        self.instrument_id
+    }
+
+    fn note_on(&mut self, note: u8, velocity: u8) {
+        self.voice.inner.note_on(note, velocity);
+    }
+
+    fn note_off(&mut self) {
+        self.voice.inner.note_off();
+    }
+
+    fn process(&mut self, left_buf: &mut [f32], right_buf: &mut [f32], sample_rate: f32) {
+        self.voice.inner.process(left_buf, right_buf, sample_rate);
+    }
+
+    fn set_pan(&mut self, pan: f32) {
+        self.voice.inner.set_pan(pan);
+    }
+
+    fn add_effect(&mut self, effect: Box<dyn MonoEffect>) {
+        self.voice.inner.add_effect(effect);
+    }
+
+    fn set_effect_parameter(&mut self, effect_index: usize, param_index: u32, value: f32) {
+        self.voice
+            .inner
+            .set_effect_parameter(effect_index, param_index, value);
+    }
+
+    fn try_handle_command(&mut self, cmd: &crate::SynthCmd) -> bool {
+        self.voice.inner.try_handle_command(cmd)
+    }
 }
--- audio_backend/src/instruments/monophonic_osc.rs
+++ audio_backend/src/instruments/monophonic_osc.rs
@@ -1,11 +1,9 @@
 use crate::id::InstrumentId;
 use crate::{instruments::VoiceSlot, OscillatorNode};
-use crate::{Envelope, InstrumentTrait, MonoEffect, MonoEffectChain, Voice, VoiceTrait, Waveform};
+use crate::{Envelope, MonoEffectChain, MonophonicInstrument, Voice, Waveform};
 
-pub struct MonophonicOscillator {
-    instrument_id: InstrumentId,
-    voice: VoiceSlot<OscillatorNode>,
-}
+
+pub type MonophonicOscillator = MonophonicInstrument<OscillatorNode>;
 
 impl MonophonicOscillator {
     pub fn new(instrument_id: InstrumentId, pan: f32, sample_rate: f32) -> Self {
@@ -51,41 +49,5 @@ impl MonophonicOscillator {
             instrument_id,
             voice,
         }
-    }
-}
-
-impl InstrumentTrait for MonophonicOscillator {
-    fn id(&self) -> crate::id::InstrumentId {
-        self.instrument_id
-    }
-
-    fn note_on(&mut self, note: u8, velocity: u8) {
-        self.voice.inner.note_on(note, velocity);
-    }
-
-    fn note_off(&mut self) {
-        self.voice.inner.note_off();
-    }
-
-    fn process(&mut self, left_buf: &mut [f32], right_buf: &mut [f32], sample_rate: f32) {
-        self.voice.inner.process(left_buf, right_buf, sample_rate);
-    }
-
-    fn set_pan(&mut self, pan: f32) {
-        self.voice.inner.set_pan(pan);
-    }
-
-    fn add_effect(&mut self, effect: Box<dyn MonoEffect>) {
-        self.voice.inner.add_effect(effect);
-    }
-
-    fn set_effect_parameter(&mut self, effect_index: usize, param_index: u32, value: f32) {
-        self.voice
-            .inner
-            .set_effect_parameter(effect_index, param_index, value);
-    }
-
-    fn try_handle_command(&mut self, cmd: &crate::SynthCmd) -> bool {
-        self.voice.inner.try_handle_command(cmd)
     }
 }
